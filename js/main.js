6d5d250c3770779ee80f11d13c35bb721ffc93f8
Dan Hacker <greenrhino77@gmail.com>
2015-01-13 09:40:10 +0000

    wip

--- js/main.js
+++ js/main.js
@@ -28,22 +28,31 @@ var controller = new ScrollMagic();
 			var ladybirdPath = [{x:62, y:-44}, {x:225, y:-70}, {x:210, y:-85}, {x:190, y:-75}, {x:206, y:-62}, {x:480, y:-130}],
 				title = $('#cover .whatword>div, #cover .theword, #cover .ladybirdword>div, #cover .heardword>div'),
 				ladybird = $('#cover .ladybird'),
-				goose = $('#cover .goose'),
-				sheep = $('#cover .sheep'),
-				horse = $('#cover .horse');
+				animals = $('#cover .animal');
+				//goose = $('#cover .goose'),
+				//sheep = $('#cover .sheep'),
+				//horse = $('#cover .horse')
+				//duck = $('#cover .duck'),
+				//cow = $('#cover .cow'),
+				//pig = $('#cover .pig');
 				
+			TweenMax.staggerFromTo(title, 0.5, {rotation:-3}, {rotation:3, yoyo:true, repeat:-1, ease:Power1.easeInOut}, 0.1);
 			
 			var t = new TimelineMax({delay:1.5})
 				.add(TweenMax.staggerFrom(title, 1, {scale:0, ease:Elastic.easeInOut}, 0.05))
-				.add(TweenMax.staggerFromTo(title, 0.5, {rotation:-3}, {rotation:3}, 0.1))
+				
 				//.add(TweenMax.staggerFrom($('#cover .theword'), 0.2, {scale:0, ease:Power2.easeOut}, 0))
 				//.add(TweenMax.staggerFrom($('#cover .ladybirdword>div'), 0.2, {scale:0, ease:Power3.easeOut}, 0.05))
 				//.add(TweenMax.staggerFrom($('#cover .heardword>div'), 0.2, {scale:0, ease:Power4.easeOut}, 0.05))
 				.add(TweenMax.from(ladybird, 0.01, {display:'block'}))
 				.add(TweenMax.from(ladybird, 4, {display:'block', bezier:{values:ladybirdPath}, onUpdate:ladybirdUpdate}))
-				.add(TweenMax.from(goose, 0.2, {right:-99, ease:Power4.easeOut}, 4.2))
-				.add(TweenMax.from(sheep, 0.2, {right:-162, ease:Power4.easeOut}, 4.3))
-				.add(TweenMax.from(horse, 0.2, {right:-150, ease:Power4.easeOut}, 4.4))
+				.add(TweenMax.staggerFrom(animals, 1, {left:0, ease:Power1.easeOut}, 0.2))
+				//.add(TweenMax.from(goose, 0.2, {right:-99, ease:Power4.easeOut}, 4.2))
+				//.add(TweenMax.from(sheep, 0.2, {right:-162, ease:Power4.easeOut}, 4.3))
+				//.add(TweenMax.from(horse, 0.2, {right:-150, ease:Power4.easeOut}, 4.4))
+				//.add(TweenMax.from(duck, 0.2, {left:-83, ease:Power4.easeOut}, 4.5))
+				///.add(TweenMax.from(cow, 0.2, {left:-175, ease:Power4.easeOut}, 4.6))
+				//.add(TweenMax.from(pig, 0.2, {left:-113, ease:Power4.easeOut}, 4.7))
 					
 				//.add(TweenMax.from($('#cover .ladybirdword', 500, {x:40, y:0, immediateRender:false})));
 			
